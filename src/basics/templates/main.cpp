1a86e60d621f2fdfaf2a5916148717dd5f0033b5
henry <henry.byrd@claricode.com>
2020-12-28 16:10:14 -0500

    Now that I've added the std::string examples, things have stopped compiling!

--- src/basics/templates/main.cpp
+++ src/basics/templates/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 
 // primary template
 template <typename T>
@@ -26,7 +27,7 @@ T sum(T *arr, int arrSize)
     T result{arr[0]};
     for (int i = 1; i < arrSize; ++i)
     {
-        result += arr[i];
+        result = add(result, arr[i]);
     }
     return result;
 }
@@ -59,10 +60,99 @@ std::pair<T, T> minMax(T *arr, int arrSize)
 // force compiler to generate a particular type of the above template method:
 // explicit instantiation
 template char max(char x, char y);
+// template std::string max(std::string x, std::string y);
 
 // explicit specialization
-template<> const char * max(const char *x, const char *y){
+
+template <>
+const char *max(const char *x, const char *y)
+{
     return strcmp(x, y) > 0 ? x : y;
+}
+// template <>
+// std::string max(std::string x, std::string y)
+// {
+//     return x > y > 0 ? x : y;
+// }
+
+template <>
+const char *min(const char *x, const char *y)
+{
+    return strcmp(x, y) < 0 ? x : y;
+}
+
+template <>
+const char *add(const char *x, const char *y)
+{
+    // with c-style strings, I'm doing dynamic memory allocation to add the strings!
+    char *result = (char *)malloc((strlen(x) + strlen(y)) * sizeof(char));
+    strcpy(result, x);
+    strcat(result, y);
+    return result;
+}
+
+// non type template arguments
+// can declare a template as taking certain constant value arguments
+template <int size>
+void print()
+{
+    // size is const, so can declare arrays with it (not variable size!)
+    char buffer[size];
+    std::cout << size << std::endl;
+}
+
+// non type template argument to specify an array size
+template <typename T, int size>
+// replace raw T pointer with an array reference
+T sum2(T (&arr)[size])
+{
+    T result{arr[0]};
+    for (int i = 1; i < size; ++i)
+    {
+        result = add(result, arr[i]);
+    }
+    return result;
+}
+
+template <typename T, int size>
+T max2(T (&arr)[size])
+{
+    T result{arr[0]};
+    for (int i = 1; i < size; ++i)
+    {
+        result = max(result, arr[i]);
+    }
+    return result;
+}
+
+template <typename T, int size>
+std::pair<T, T> minMax2(T (&arr)[size])
+{
+    std::pair<T, T> result{arr[0], arr[0]};
+
+    for (int i = 1; i < size; ++i)
+    {
+        result.first = min(result.first, arr[i]);
+        result.second = max(result.second, arr[i]);
+    }
+
+    return result;
+}
+
+// due to dynamic memory allocation, summing array of strings requires a little extra work
+template<int size>
+const char * sum2(const char *(&arr)[size])
+{
+    char *result = (char *)malloc(size * sizeof(char));
+    strcpy(result, arr[0]);
+    char *temp{};
+    for (int i = 1; i < size; ++i)
+    {
+        temp = result;
+        result = (char *)add<const char *>(result, arr[i]);
+        free(temp);
+    }
+    return result;
 }
 
 int main()
@@ -91,6 +181,37 @@ int main()
     const char *a{"A"};
     auto s = max(a, b);
     std::cout << "max of " << a << " and " << b << ": " << s << std::endl;
+
+    // constant value non-type template ok
+    print<3>();
+    const int cval{5};
+    print<cval>();
+    // non-const value not ok - the compiler needs to know the value of the non-type template argument
+    // int val{5};
+    // print<val>();
+
+    std::cout << "using non-type template arguments to specify array size: " << std::endl;
+    // using non-type template arguments to omit the arrSize variable
+    int arr2[]{1, 2, 3, 4, 5};
+    std::cout << "sum " << sum2(arr2) << std::endl;
+    std::cout << "max " << max2(arr2) << std::endl;
+    auto result2 = minMax2(arr2);
+    std::cout << "minmax " << result2.first << ":" << result2.second << std::endl;
+
+    const char *arr3[]{"E", "D", "C", "B", "A"};
+    std::cout << "max " << max2(arr3) << std::endl;
+    auto result3 = minMax2(arr3);
+    std::cout << "minmax " << result3.first << ":" << result3.second << std::endl;
+
+    std::cout << "sum " << sum2(arr3) << std::endl;
+
+    // all of a sudden, if I add a test case for std::string, I can't compile because the templates are ambiguous
+    std::string arr4[5] {"E", "D", "C", "B", "A"};
+    std::cout << "sum " << sum2(arr4) << std::endl;
+    std::cout << "max " << max2(arr4) << std::endl;
+    auto result4 = minMax2(arr4);
+    std::cout << "minmax " << result4.first << ":" << result4.second << std::endl;
+
 
     return 0;
 }
