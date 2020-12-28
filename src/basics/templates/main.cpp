8323b6a3ddfead980417bb9f2b2f2830ea57eea2
henry <henry.byrd@claricode.com>
2020-12-28 15:11:22 -0500

    Added some ways of explicitly specifying or specializing types

--- src/basics/templates/main.cpp
+++ src/basics/templates/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <cstring>
 
+// primary template
 template <typename T>
 T max(T x, T y)
 {
     return x > y ? x : y;
 }
+
+// force compiler to generate a particular type of the above template method:
+// explicit instantiation
+template char max(char x, char y);
 
 template <typename T>
 T min(T x, T y)
@@ -54,11 +60,13 @@ std::pair<T, T> minMax(T *arr, int arrSize)
     return result;
 }
 
+// explicit specialization
+template<> const char * max(const char *x, const char *y){
+    return strcmp(x, y) > 0 ? x : y;
+}
+
 int main()
 {
-    // following doesn't work - needs same type!
-    // std::cout << max(1, 2.0) << std::endl;
-
     std::cout << "max " << max(1, 2) << std::endl;
     std::cout << "min " << min(1, 2) << std::endl;
     std::cout << "add " << add(1, 2) << std::endl;
@@ -68,6 +76,21 @@ int main()
     std::cout << "max " << max(arr, 5) << std::endl;
     auto result = minMax(arr, 5);
     std::cout << "minmax " << result.first << ":" << result.second << std::endl;
+
+    // following doesn't work - needs same type!
+    // std::cout << max(1, 2.0) << std::endl;
+    // but we can do this:
+    std::cout << "specify float " << max<float>(1, 2.3) << std::endl;
+
+    // can also create a pointer to the method:
+    double (*m)(double, double) = max;
+    std::cout << "method constructed by pointer " << m(2.5, 2) << std::endl;
+
+    // the following only prints the correct result because of the explicit specialization for const char *
+    const char *b{"B"};
+    const char *a{"A"};
+    auto s = max(a, b);
+    std::cout << "max of " << a << " and " << b << ": " << s << std::endl;
 
     return 0;
 }
