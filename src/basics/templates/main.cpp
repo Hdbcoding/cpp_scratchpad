3afa5a0967cb92ebeca6eeef56bf587f744a505b
henry <henry.byrd@claricode.com>
2020-12-28 20:41:45 -0500

    Fixed one of the problems with a rename

--- src/basics/templates/main.cpp
+++ src/basics/templates/main.cpp
@@ -5,14 +5,14 @@
 #pragma region max
 
 template <typename T>
-T max(T x, T y)
+T getMax(T x, T y)
 {
     return x > y ? x : y;
 }
 
 // // explicit specialization
 template <>
-const char *max(const char *x, const char *y)
+const char *getMax(const char *x, const char *y)
 {
     return strcmp(x, y) > 0 ? x : y;
 }
@@ -24,7 +24,7 @@ T max(T *arr, int arrSize)
     T result{arr[0]};
     for (int i = 1; i < arrSize; ++i)
     {
-        result = max(result, arr[i]);
+        result = getMax(result, arr[i]);
     }
     return result;
 }
@@ -36,7 +36,7 @@ T max(T (&arr)[size])
     T result{arr[0]};
     for (int i = 1; i < size; ++i)
     {
-        result = max(result, arr[i]);
+        result = getMax(result, arr[i]);
     }
     return result;
 }
@@ -46,13 +46,13 @@ T max(T (&arr)[size])
 #pragma region min
 
 template <typename T>
-T min(T x, T y)
+T getMin(T x, T y)
 {
     return x > y ? y : x;
 }
 
 template <>
-const char *min(const char *x, const char *y)
+const char *getMin(const char *x, const char *y)
 {
     return strcmp(x, y) < 0 ? x : y;
 }
@@ -68,8 +68,8 @@ std::pair<T, T> minMax(T *arr, int arrSize)
 
     for (int i = 1; i < arrSize; ++i)
     {
-        result.first = min(result.first, arr[i]);
-        result.second = max(result.second, arr[i]);
+        result.first = getMin(result.first, arr[i]);
+        result.second = getMax(result.second, arr[i]);
     }
 
     return result;
@@ -82,8 +82,8 @@ std::pair<T, T> minMax(T (&arr)[size])
 
     for (int i = 1; i < size; ++i)
     {
-        result.first = min(result.first, arr[i]);
-        result.second = max(result.second, arr[i]);
+        result.first = getMin(result.first, arr[i]);
+        result.second = getMax(result.second, arr[i]);
     }
 
     return result;
@@ -144,8 +144,8 @@ const char *sum(const char *(&arr)[size])
 int main()
 {
     std::cout << "simple integers" << std::endl;
-    std::cout << "max " << max(1, 2) << std::endl;
-    std::cout << "min " << min(1, 2) << std::endl;
+    std::cout << "max " << getMax(1, 2) << std::endl;
+    std::cout << "min " << getMin(1, 2) << std::endl;
     std::cout << "add " << add(1, 2) << std::endl;
 
     std::cout << "array of integers" << std::endl;
@@ -158,16 +158,16 @@ int main()
     // following doesn't work - needs same type!
     // std::cout << max(1, 2.0) << std::endl;
     // but we can do this:
-    std::cout << "specify float " << max<float>(1, 2.3) << std::endl;
+    std::cout << "specify float " << getMax<float>(1, 2.3) << std::endl;
 
     // can also create a pointer to the method:
-    double (*m)(double, double) = max;
+    double (*m)(double, double) = getMax;
     std::cout << "method constructed by pointer " << m(2.5, 2) << std::endl;
 
     // the following only prints the correct result because of the explicit specialization for const char *
     const char *b{"BCD"};
     const char *a{"ABC"};
-    auto s = max(a, b);
+    auto s = getMax(a, b);
     std::cout << "max of " << a << " and " << b << ": " << s << std::endl;
 
     std::cout << "using non-type template arguments to specify array size: " << std::endl;
@@ -191,13 +191,14 @@ int main()
     std::string strB{"BCD"};
     std::cout << "sum " << add(strA, strB) << std::endl;
     // add is ok, but min/max cause issues
-    // std::cout << "min " << min(strA, strB) << std::endl;
+    std::cout << "min " << getMin(strA, strB) << std::endl;
+    std::cout << "max " << getMax(strA, strB) << std::endl;
 
     std::string arr4[5] {"EFG", "DEF", "CDE", "BCD", "ABC"};
     std::cout << "sum " << sum(arr4) << std::endl;
-    // std::cout << "max " << max(arr4) << std::endl;
-    // auto result4 = minMax(arr4);
-    // std::cout << "minmax " << result4.first << ":" << result4.second << std::endl;
+    std::cout << "max " << max(arr4) << std::endl;
+    auto result4 = minMax(arr4);
+    std::cout << "minmax " << result4.first << ":" << result4.second << std::endl;
 
     // Current total output:
     // max 5
